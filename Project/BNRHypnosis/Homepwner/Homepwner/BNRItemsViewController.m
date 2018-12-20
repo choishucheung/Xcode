0d91dbad5b9ceb2b6f8da273cf6ede7440c5746d
choishucheung <choishucheung@qq.com>
2018-12-20 21:05:24 +0800

    20181220 可以添加行 删除行删除事件未响应

--- Project/BNRHypnosis/Homepwner/Homepwner/BNRItemsViewController.m
+++ Project/BNRHypnosis/Homepwner/Homepwner/BNRItemsViewController.m
@@ -9,13 +9,17 @@
 #import "BNRItemsViewController.h"
 #import "BNRItemStore.h"
 #import "BNRItem.h"
+@interface BNRItemsViewController()
+@property (nonatomic,strong) IBOutlet UIView *headerView;
+@end
+
 @implementation BNRItemsViewController
 - (instancetype)init{
     self = [super initWithStyle:UITableViewStylePlain];
     if(self){
-        for(int i=0;i<5;i++){
-            [[BNRItemStore sharedStore]createItem];
-        }
+        //for(int i=0;i<5;i++){
+        //    [[BNRItemStore sharedStore]createItem];
+        //}
     }
     return self;
 }
@@ -28,7 +32,8 @@
     return [[[BNRItemStore sharedStore]allItem]count];
 }
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    UITableViewCell *cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"UITableViewCell"];
+    //UITableViewCell *cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"UITableViewCell"];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell" forIndexPath:indexPath];
     NSArray *items = [[BNRItemStore sharedStore]allItem];
     BNRItem *item = items[indexPath.row];
     cell.textLabel.text = [item description];
@@ -37,6 +42,36 @@
 - (void)viewDidLoad{
     [super viewDidLoad];
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"UITableViewCell"];
+    UIView *header = self.headerView;
+    [self.tableView setTableHeaderView:header];
 }
 
+
+- (IBAction)addNewItem:(id)sender
+{
+    //NSInteger lastRow = [self.tableView numberOfRowsInSection:0];
+    BNRItem *newItem =[[BNRItemStore sharedStore]createItem];
+    NSInteger lastRow = [[[BNRItemStore sharedStore]allItem]indexOfObject:newItem];
+    
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:lastRow inSection:0];
+    [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationTop];
+}
+
+-(IBAction)togglEditngMode:(id)sender
+{
+    if(self.isEditing){
+        [sender setTitle:@"Edit" forState:UIControlStateNormal];
+        [self setEditing:NO animated:YES];
+    }else{
+        [sender setTitle:@"Done" forState:UIControlStateNormal];
+        [self setEditing:YES animated:NO];
+    }
+}
+
+-(UIView *)headerView{
+    if(!_headerView){
+        [[NSBundle mainBundle] loadNibNamed:@"HeaderView" owner:self options:nil];
+    }
+    return _headerView;
+}
 @end
