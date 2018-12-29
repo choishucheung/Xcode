3d9769dbd1392cb0c0e4f9e513346500c0fea408
choishucheung <choishucheung@qq.com>
2018-12-29 18:15:46 +0800

    2018.12.29

---
+++ Project/BNRHypnosis/Homepwner/Homepwner/BNRDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRDetailViewController.h
+//  Homepwner
+//
+//  Created by v_caisxcai on 2018/12/29.
+//  Copyright © 2018 choishucheung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRDetailViewController : UIViewController
+
+@end
