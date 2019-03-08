b5d80674bfa94547abc5552d4e045253c1494b7f
choishucheung <choishucheung@qq.com>
2019-03-08 17:15:04 +0800

    Create Unity shader simple project 2019.03.08

--- Project/BNRHypnosis/TouchTracker/BNRDrawView.m
+++ Project/BNRHypnosis/TouchTracker/BNRDrawView.m
@@ -10,7 +10,8 @@
 #import "BNRLine.h"
 
 @interface BNRDrawView()
-@property (nonatomic,strong)BNRLine *currentLine;
+//@property (nonatomic,strong)BNRLine *currentLine;
+@property (nonatomic,strong)NSMutableDictionary *linesInProgress;
 @property (nonatomic,strong)NSMutableArray *finishedLines;
 @end
 
@@ -18,8 +19,14 @@
 -(instancetype)initWithFrame:(CGRect)frame{
     self = [super initWithFrame:frame];
     if(self){
+        self.linesInProgress = [[NSMutableDictionary alloc]init];
         self.finishedLines = [[NSMutableArray alloc]init];
         self.backgroundColor = [UIColor grayColor];
+        self.multipleTouchEnabled = YES;
+        UITapGestureRecognizer *doubleTapRecognizer = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(doubleTap:)];
+        doubleTapRecognizer.numberOfTapsRequired = 2;
+        doubleTapRecognizer.delaysTouchesBegan = YES;
+        [self addGestureRecognizer:doubleTapRecognizer];
     }
     return self;
 }
@@ -38,35 +45,69 @@
     for(BNRLine *line in self.finishedLines){
         [self strokeLine:line];
     }
-    if(self.currentLine){
-        [[UIColor redColor]set];
-        [self strokeLine:self.currentLine];
+    [[UIColor redColor]set];
+    for(NSValue *key in self.linesInProgress){
+        [self strokeLine:self.linesInProgress[key]];
     }
-    
+//    if(self.linesInProgress ){
+//        [[UIColor redColor]set];
+//        [self strokeLine:self.currentLine];
+//    }
+//
 }
 
 -(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
-    UITouch *t = [touches anyObject];
-    CGPoint location = [t locationInView:self];
-    self.currentLine = [[BNRLine alloc]init];
-    self.currentLine.begin = location;
-    self.currentLine.end = location;
+    NSLog(@"%@",NSStringFromSelector(_cmd));
+    for(UITouch *t in touches){
+        CGPoint location = [t locationInView:self];
+        BNRLine *line = [[BNRLine alloc]init];
+        line.begin = location;
+        line.end = location;
+        NSValue *key = [NSValue valueWithNonretainedObject:t];
+        self.linesInProgress[key] = line;
+    }
+//    UITouch *t = [touches anyObject];
+//    CGPoint location = [t locationInView:self];
+//    self.currentLine = [[BNRLine alloc]init];
+//    self.currentLine.begin = location;
+//    self.currentLine.end = location;
     [self setNeedsDisplay];
 }
 
 -(void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
-    UITouch *t = [touches anyObject];
-    CGPoint location = [t locationInView:self];
-    self.currentLine.end= location;
+    NSLog(@"%@",NSStringFromSelector(_cmd));
+    for(UITouch *t in touches){
+        NSValue *key = [NSValue valueWithNonretainedObject:t];
+        BNRLine *line = self.linesInProgress[key];
+        line.end = [t locationInView:self];
+    }
+//    UITouch *t = [touches anyObject];
+//    CGPoint location = [t locationInView:self];
+//    self.currentLine.end= location;
     [self setNeedsDisplay];
 }
 
 -(void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-    [self.finishedLines addObject:self.currentLine];
-    self.currentLine = nil;
+  
+    NSLog(@"%@",NSStringFromSelector(_cmd));
+    for(UITouch *t in touches){
+        NSValue *key = [NSValue valueWithNonretainedObject:t];
+        BNRLine *line = self.linesInProgress[key];
+        [self.finishedLines addObject:line];
+        [self.linesInProgress removeObjectForKey:key];
+    }
+//    self.currentLine = nil;
     [self setNeedsDisplay];
+}
+
+-(void)doubleTap:(UIGestureRecognizer *)gr{
+    NSLog(@"Recognized Double Tap");
+    [self.linesInProgress removeAllObjects];
+    [self.finishedLines removeAllObjects];
+    [self setNeedsDisplay];
+    
 }
 
 @end
