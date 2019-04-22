10928e5222c30a87dcece43e0e5df61f26f87719
choishucheung <choishucheung@qq.com>
2019-04-22 20:05:40 +0800

    修改

--- Project/PhuketTour/PhuketTour/PhuketTour/AppDelegate.m
+++ Project/PhuketTour/PhuketTour/PhuketTour/AppDelegate.m
@@ -17,6 +17,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc]initWithFrame:[[UIScreen mainScreen]bounds]];
+    UINavigationController *navigationController = [[UINavigationController alloc]initWithRootViewController:[[UIViewController alloc] initWithNibName:@"ViewController" bundle:nil]];
+    self.window.rootViewController = navigationController;
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
